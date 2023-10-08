793746b323fadb524ca6f18521fab15f2307c34d
ubilby <ubilby@gmail.com>
2023-10-08 15:52:08 +0400

    finish tag

--- backend/backend/permissions.py
+++ backend/backend/permissions.py
@@ -59,3 +59,11 @@ class IsAdmin(permissions.BasePermission):
                 request.user.is_admin or request.user.is_superuser
             )
         )
+
+
+class ReadOnly(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return request.method in permissions.SAFE_METHODS
+
+    def has_object_permission(self, request, view, obj):
+        return request.method in permissions.SAFE_METHODS
