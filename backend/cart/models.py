d0dfb882853dfd2c06946626ff22ab196723bda0
ubilby <ubilby@gmail.com>
2023-10-18 15:23:15 +0400

    add download shop list

--- backend/cart/models.py
+++ backend/cart/models.py
@@ -8,3 +8,6 @@ class Cart(models.Model):
     user = models.ForeignKey(
         CustomUser, on_delete=models.CASCADE, related_name='cart_recipes')
     recipe = models.ForeignKey(Recipes, on_delete=models.CASCADE)
+
+    class Meta:
+        default_related_name = 'cart_recipes'
--- backend/recipes/urls.py
+++ backend/recipes/urls.py
@@ -2,7 +2,7 @@ from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
 from .views import RecipesViewSet
-from favorites.views import FavoriteView
+from favorites.views import FavoriteView, FileView
 from cart.views import CartView
 
 app_name = 'recipes'
@@ -10,6 +10,9 @@ router = DefaultRouter()
 router.register(r'', RecipesViewSet)
 
 urlpatterns = [
+    # download_shopping_cart/
+    path('download_shopping_cart/',
+         FileView.as_view(), name='download_file'),
     path('<int:pk>/favorite/', FavoriteView.as_view(), name='favorite'),
     path('<int:pk>/shopping_cart/', CartView.as_view(), name='cart'),
     path('', include(router.urls)),
