fe5191490c53dac5c14a546df4ed85ca715a1dcb
ubilby <ubilby@gmail.com>
2023-10-19 12:00:45 +0400

    little fixies

--- backend/ingredients/models.py
+++ backend/ingredients/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Ingredients(models.Model):
     id = models.BigAutoField(primary_key=True)
     name = models.CharField(max_length=100)
-    measurement_unit = models.CharField(max_length=2)
+    measurement_unit = models.CharField(max_length=6)
 
     def __str__(self):
         return self.name
