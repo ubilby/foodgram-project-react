28716bc63aaf82fbe7d62a094cadd3e45d751c86
ubilby <ubilby@gmail.com>
2023-10-21 14:48:32 +0400

    add limit parametr to users pagination

--- backend/users/views.py
+++ backend/users/views.py
@@ -3,7 +3,9 @@ from rest_framework import status
 from rest_framework.generics import CreateAPIView, RetrieveAPIView
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.response import Response
+
 
 from .models import CustomUser
 from .serializers import (ChangePasswordSerializer, CustomUserSerializer,
@@ -22,6 +24,7 @@ class UserProfileView(RetrieveAPIView):
 class UserCreateView(ModelViewSet):
     queryset = CustomUser.objects.all()
     permission_classes = [AllowAny, ]
+    pagination_class = LimitOffsetPagination
 
     def get_serializer_class(self):
         if self.request.method == 'POST':
