86df0830e92292b6fdd4fd7913038f1574a45f31
ubilby <ubilby@gmail.com>
2023-10-21 15:14:46 +0400

    fix ingredients permissions

--- backend/ingredients/views.py
+++ backend/ingredients/views.py
@@ -1,10 +1,21 @@
+from rest_framework import status
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.response import Response
 
 from .models import Ingredients
 from .serializers import IngredientsSerializer
+from backend.permissions import IsAdminOrReadOnly
 
 
 class IngredientsViewSet(ModelViewSet):
     queryset = Ingredients.objects.all()
     serializer_class = IngredientsSerializer
     pagination_class = None
+    permission_classes = [IsAdminOrReadOnly, ]
+
+    def http_method_not_allowed(self, request, *args, **kwargs):
+        """
+        Переопределяем метод для возвращения 405 Method Not Allowed
+        вместо 403 Forbidden.
+        """
+        return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
