7d0b7e8296ed4cf5e59fcf177be21fd6247664b2
ubilby <ubilby@gmail.com>
2023-10-23 21:40:23 +0400

    remove roles

--- backend/users/models.py
+++ backend/users/models.py
@@ -5,28 +5,11 @@ from backend.validators import username_validator
 
 
 class Account(AbstractUser):
-    USERNAME_FIELD = 'username'
-    EMAIL_FIELD = 'email'
-    REQUIRED_FIELDS = ['email']
-
-    ROLE_USER = 'user'
-    ROLE_ADMIN = 'admin'
-
-    ROLE_CHOICES = (
-        (ROLE_USER, 'user'),
-        (ROLE_ADMIN, 'admin'),
-    )
-
     username = models.SlugField(
         unique=True,
         validators=[username_validator],
     )
     email = models.EmailField(unique=True)
-    role = models.CharField(
-        max_length=64,
-        choices=ROLE_CHOICES,
-        default=ROLE_USER
-    )
     first_name = models.CharField(max_length=150)
     last_name = models.CharField(max_length=150)
     password = models.CharField(max_length=100)
@@ -41,12 +24,6 @@ class Account(AbstractUser):
                 name="name_not_me"
             )
         ]
-
-    @property
-    def is_admin(self):
-        return self.role == (self.ROLE_ADMIN
-                             or self.is_superuser
-                             or self.is_staff)
 
     def __str__(self):
         return self.username
