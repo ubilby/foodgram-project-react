7de10b011ca0a2e4b0c053dc5931a2a2f78d7aba
ubilby <ubilby@gmail.com>
2023-10-24 20:54:39 +0400

    add some models to admin

---
+++ backend/users/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Account
+from subscribes.models import Subscribe
+
+
+admin.site.register(Account, admin.ModelAdmin)
+admin.site.register(Subscribe, admin.ModelAdmin)
